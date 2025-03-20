25357b76dd5f2a96ec0f0d147d46dea87c2df444
Xenon <alguirre2@gmail.com>
2025-03-20 13:44:31 -0600

    15 sign up

--- app/components/modals/SignupModal.tsx
+++ app/components/modals/SignupModal.tsx
@@ -2,37 +2,86 @@
 import Modal from './Modal'
 
 import {useState} from "react"
+import { useRouter } from 'next/navigation';
 
 import useSignupModal from '@/app/hooks/useSignupModal';
 import CustomButton from '../forms/CustomButton';
+import apiService from '@/app/services/apiService';
+import { handleLogin } from '@/app/lib/acctions';
 
 const SignupModal= () => {
+
+    //
+    //variavles
+    const router = useRouter()
     const SignupModal=useSignupModal()
+    const [email, setEmail]= useState('');
+    const [password1,setPassword1]= useState('');
+    const [password2,setPassword2]= useState('');
+    const [errors,setErrors]=useState<string[]>([]);
+    //
+    // sumbit funcion
+
+    const submitSignup= async () =>{
+        const formData= {
+            email:email,
+            password1:password1,
+            password2:password2,
+        }
+        const response = await apiService.post('/api/auth/register/', JSON.stringify(formData))
+
+        if(response.access){
+            handleLogin(response.user.pk, response.access,response.refresh)
+
+            SignupModal.close()
+
+            router.push('/')
+
+        }else {
+            const tmpErrors:string[]= Object.values(response).map((error:any)=>{
+                    return error;
+            })
+            setErrors(tmpErrors)
+        }
+    }
 
     const content = (
 <>
-        <h2 className='mb-6 text-2xl'>Welcle ty¿o Django, plase log </h2>
         
-        <form className='space-y-4'>
+        <form 
+        action={submitSignup}
+        className='space-y-4'>
             <input 
+            onChange={(e)=>setEmail(e.target.value)}
             placeholder='Your emial adres'
             type="email"  className='w-full h-[54px] px-4 border border-gray-300 rounded-xl'/> 
+            
             <input 
+             onChange={(e)=>setPassword1(e.target.value)}
             placeholder='Your password'
             type="password"  className='w-full h-[54px] px-4 border border-gray-300 rounded-xl'/> 
        
             <input 
+            onChange={(e)=>setPassword2(e.target.value)}
+
             placeholder='Repeat your password'
             type="password"  className='w-full h-[54px] px-4 border border-gray-300 rounded-xl'/> 
        
-       
-        <div className='p-5 bg-airbnb text-white rounded-xl opacity-80'>
-            the error enssaje
+        {errors.map((error,index)=>{
+        return(
+        <div
+            key={`error_${index}`}
+        className='p-5 bg-airbnb text-white rounded-xl opacity-80'>
+            {error}
         </div>
+
+        )
+    
+          })}
 
         <CustomButton
             label='Submit'
-            onClick={()=> console.log('Test')}
+            onClick={submitSignup}
         ></CustomButton>
         </form>
 </>
