4b861864b3c35996eb1deeef69f6b63cc272e158
Xenon <alguirre2@gmail.com>
2025-03-28 22:52:57 -0600

    35 fix the refresh tokens

--- app/lib/acctions.ts
+++ app/lib/acctions.ts
@@ -2,6 +2,52 @@
 
 import { cookies } from "next/headers";
 
+export async function handleRefresh() {
+    console.log('handleRefresh')
+
+    const refreshToken=await getRefreshToken()
+
+    const token= await fetch("http://localhost:8000/api/auth/token/refresh/",{
+        method:'POST',
+        body:JSON.stringify({
+            refresh:refreshToken
+        }),
+        headers:{
+            'Accetp': 'application/json',
+            'Content-Type':'application/json'
+        }
+
+
+    })
+    .then(response => response.json())
+    .then(async json => {
+        console.log('response - refresH:',json)
+
+        if (json.access){
+            (await cookies()).set('session_access_token', json.access,
+                {
+                    httpOnly:true,
+                    secure: process.env.NODE_ENV==='production',
+                    maxAge: 60*60,
+                    path:'/'
+                });
+
+                return json.access
+            
+        } else {
+            resetAuthCookies()
+        }
+    })
+
+    .catch((error)=>{
+        console.log('error', error);
+        resetAuthCookies()
+    })
+
+    return token
+    
+}
+
 export async function handleLogin(userId:string,accesToken:string,refreshToken:string ) {
     
      (await cookies()).set('session_userid', userId,
@@ -48,6 +94,17 @@ export async function getAccessToken() {
 
     let accesToken= (await cookies()).get('session_access_token')?.value
 
+    if(!accesToken){
+        accesToken=await handleRefresh()
+    }
+
     return accesToken
+    
+}
+export async function getRefreshToken() {
+
+    let refreshToken= (await cookies()).get('session_refresh_token')?.value
+
+    return refreshToken
     
 }
